69bbd2e0b6320fe0b8d9aad5ed45dc8c2751617a
mazlumabul <mazlumabul10@gmail.com>
2021-06-13 04:48:05 +0300

    GLobal exception Handler

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/UsersController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/UsersController.java
@@ -1,32 +1,59 @@
 package kodlamaio.northwind.api.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.UserService;
 import kodlamaio.northwind.core.entities.User;
-import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.ErrorDataResult;
 
 @RestController
-@RequestMapping(value ="/api/users")
+@RequestMapping(value = "/api/users")
 public class UsersController {
-	
+
 	private UserService userService;
-	
+
+	@Autowired
 	public UsersController(UserService userService) {
 		super();
 		this.userService = userService;
 	}
-	
-	@PostMapping(value ="/add")	
-	public ResponseEntity<?> add(@RequestBody User user) {
-		// işlem başarısız da olabilir. 
-		// <?>  onun şimdilk ne dönceğine karar vermke istemiyorum.işlem sonucuna göre ekle 
+
+	@PostMapping(value = "/add")
+	public ResponseEntity<?> add(@Valid @RequestBody User user) {
+		// işlem başarısız da olabilir.
+		// <?> onun şimdilk ne dönceğine karar vermke istemiyorum.işlem sonucuna göre
+		// ekle
 		return ResponseEntity.ok(this.userService.add(user));
 	}
-	//2.43
-	
+
+	// Global hata yakalam işlemi : Aop
+	// GLobal exception Handler
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST) // 500 hatası dönsün
+	public ErrorDataResult<Object> handleValidationException
+	(MethodArgumentNotValidException exceptions) {
+		Map<String, String> validationErrors = new HashMap<String, String>();
+		for (FieldError fieldError : exceptions.getBindingResult().getFieldErrors()) {
+			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		ErrorDataResult<Object> errors = new ErrorDataResult<Object>(validationErrors, "Doğrulama Hataları");
+		return errors;
+
+	}
+
 }
